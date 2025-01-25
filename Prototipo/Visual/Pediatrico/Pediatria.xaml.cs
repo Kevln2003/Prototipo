0551fa4e87bc4df57293d51928bc9012ec9c0293
Kevln2003 <kevin.cep3d4@gmail.com>
2025-01-24 22:08:29 -0500

    No se porque no carga el archivo completo Add project files.

--- Prototipo/Visual/Pediatrico/Pediatria.xaml.cs
+++ Prototipo/Visual/Pediatrico/Pediatria.xaml.cs
@@ -33,7 +33,7 @@ namespace Prototipo.Visual.Pediatrico
         {
             if (pageType == "Formulario")
             {
-                ContentFrame.Navigate(typeof(FormularioPediatrico));
+                ContentFrame.Navigate(typeof(OpcionesPediatria));
 
 
             }
